57e2ea5937251f19b1fc39cb6e321f4d7c75b349
AbrMa <abrma98@gmail.com>
2023-05-28 19:57:44 -0600

    Estructura proyecto

---
+++ api/src/index.js
@@ -0,0 +1,8 @@
+const app = require('./server')
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`[Servidor iniciado en el puerto ${PORT}]`)
+    console.log(`[http://localhost:${PORT}]`)
+})
