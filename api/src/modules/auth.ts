69b605f4a211ba69cd83268c8e36734abf24d0f4
AbrMa <abrma98@gmail.com>
2023-06-04 17:11:36 -0600

    Validación del token

--- api/src/modules/auth.ts
+++ api/src/modules/auth.ts
@@ -8,11 +8,36 @@ export const createJWT = (user) => {
   return token;
 }
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
     const bearer = req.headers.authorization
 
     if (!bearer) {
         res.status(401)
+        res.json({
+            message: "Acceso no autorizado"
+        })
+        return
+    }
+
+    const [, token] = bearer.split(" ")
+    if (!token) {
+        console.log("here")
+        res.status(401)
+        res.json({
+            message: "Acceso no autorizado"
+        })
+        return;
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = payload
+        console.log(payload)
+        next()
+        return
+    } catch (e) {
+        console.error(e);
+        res.status(401);
         res.json({
             message: "Acceso no autorizado"
         })
