5193a667b67462f6aeebfd51bd849b2884fef563
AbrMa <abrma98@gmail.com>
2023-06-04 16:05:35 -0600

    Creación del JWT

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,6 +3,9 @@ import router from './router'
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.status(200)
     res.json({
